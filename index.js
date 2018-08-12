6211d8c1c6784f9422e30709f7ecbade310f73c7
p-duke <peter.duke@bounceexchange.com>
2018-08-12 18:05:54 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+// vast majority of projects use a single app object
+const app = express();
+
+// app.get creates new route handler to watch for '/'
+// you can create app.<http_verb>
+app.get('/', (req, res) => {
+    res.send({ hi: 'there' });
+});
+
+// Heroku can pass us runtime config based on environment
+// this variable wouldn't work in dev environment so short circuit
+const PORT = process.env.PORT || 5000;
+// telling node to listen to port 5000
+app.listen(PORT);
