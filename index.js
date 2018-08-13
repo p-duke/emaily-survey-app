cb7da37c3dcc95a3e28028db48f5907335a90069
p-duke <peter.duke@bounceexchange.com>
2018-08-13 10:19:30 -0400

    install passport

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 // vast majority of projects use a single app object
 const app = express();
+// passport.use is a generic register to be aware of new strategy
+passport.use(new GoogleStrategy(() => {}));
 
 // app.get creates new route handler to watch for '/'
 // you can create app.<http_verb>
-app.get('/', (req, res) => {
-    res.send({ by: 'buddy' });
-});
 
 // Heroku can pass us runtime config based on environment
 // this variable wouldn't work in dev environment so short circuit
