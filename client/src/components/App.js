b195f2e53bdca50bb33e9ef52ae6297d4b340a29
p-duke <peter.duke@bounceexchange.com>
2018-09-04 09:57:17 -0400

    Setup react router and install materialize-css

---
+++ client/src/components/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Header from "./Header";
+
+const Dashboard = () => <h2>Dashboard </h2>;
+const SurveyNew = () => <h2>SurveyNew </h2>;
+const Landing = () => <h2>Landing </h2>;
+
+/*
+BrowserRouter must have one child - cannot have two divs
+use exact = true for strict path matching
+*/
+const App = () => {
+  return (
+    <div className="container">
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route path="/surveys/new" component={SurveyNew} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,19 @@
+// When not importing JS files you need to specify the extension
+// Also when you don't specify a relative path like ./ webpack will assume you are referring to node modules
+// We don't assign the css to a variable because that does nothing. So just simply import
+import "materialize-css/dist/css/materialize.min.css";
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reducers from "./reducers";
 
-ReactDOM.render();
+const store = createStore(reducers, {}, applyMiddleware());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
