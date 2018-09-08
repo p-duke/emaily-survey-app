600c12276337d1c1e3616934f4687bb441fb85c0
p-duke <peter.duke@bounceexchange.com>
2018-09-08 18:13:41 -0400

    Configure stripe keys

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,9 @@
+// None of these keys should ever be present on the client frontend - only backend
 module.exports = {
-    googleClientID: process.env.GOOGLE_CLIENT_ID,
-    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    mongooseURI: process.env.MONGO_URI,
-    cookieKey: process.env.COOKIE_KEY
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongooseURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+  stripePublishableKey: process.env.STRIPE_PUBLISABLE_KEY,
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY
 };
