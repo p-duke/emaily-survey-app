1ad5d94dffc69cdd6e6c27a0dfd8a8ff083af5cc
p-duke <peter.duke@bounceexchange.com>
2018-09-09 16:58:19 -0400

    Wire up redux form

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from "redux";
+import { reducer as reduxForm } from "redux-form";
 import authReducer from "./authReducer";
 
+// to use redux form we need to use the 'reducer' on a key called form specifically
 export default combineReducers({
-  auth: authReducer
+  auth: authReducer,
+  form: reduxForm
 });
