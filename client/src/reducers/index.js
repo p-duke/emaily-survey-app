e6668962feea42e21f853f7ce47d3c83b8055deb
p-duke <peter.duke@bounceexchange.com>
2018-09-15 10:18:28 -0400

    refactor sendgrid webhook and create surveys reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import { reducer as reduxForm } from "redux-form";
 import authReducer from "./authReducer";
+import surveysReducer from "./surveysReducer";
 
 // to use redux form we need to use the 'reducer' on a key called form specifically
 export default combineReducers({
   auth: authReducer,
-  form: reduxForm
+  form: reduxForm,
+  surveys: surveysReducer
 });
--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -12,22 +12,50 @@ const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 const Survey = mongoose.model("surveys");
 
 module.exports = app => {
-  app.get("/api/surveys/thanks", (req, res) => {
+  app.get("/api/surveys", requireLogin, async (req, res) => {
+    const surveys = await Survey.find({ _user: req.user.id }).select({
+      recipients: false
+    });
+    res.send(surveys);
+  });
+
+  app.get("/api/surveys/:surveyId/:choice", (req, res) => {
     res.send("Thanks for voting!");
   });
 
   app.post("/api/surveys/webhooks", (req, res) => {
-    const events = _.map(req.body, ({ email, url }) => {
-      const pathname = new URL(url).pathname;
-      // Path is a regex that you give it a pattern and it will construct and object with those :names
-      const p = new Path("/api/surveys/:surveyId/:choice");
-      const match = p.test(pathname);
-      if (match) {
-        return { email, surveyId: match.surveyId, choice: match.choice };
-      }
-    });
-    const compactEvents = _.compact(events);
-    const uniqueEvents = _.uniqBy(compactEvents, "email", "surveyId");
+    // Path is a regex that you give it a pattern and it will construct and object with those :names
+    const p = new Path("/api/surveys/:surveyId/:choice");
+    _.chain(req.body)
+      .map(({ email, url }) => {
+        const match = p.test(new URL(url).pathname);
+        if (match) {
+          return { email, surveyId: match.surveyId, choice: match.choice };
+        }
+      })
+      .compact()
+      .uniqBy("email", "surveyId")
+      .each(({ surveyId, email, choice }) => {
+        // find the record and update
+        // $inc is a mongo operator for increment - choice is 'yes' / 'no' and is going to interpolate choice
+        // $set is another operator that is looking in the recipients subdocument, finding that specific index
+        // and setting the responded attr to true
+        // not using async await on updateOne because it doesn't matter what we return to sendgrid
+        Survey.updateOne(
+          {
+            _id: surveyId,
+            recipients: {
+              $elemMatch: { email: email, responded: false }
+            }
+          },
+          {
+            $inc: { [choice]: 1 },
+            $set: { "recipients.$.responded": true },
+            lastResponded: new Date()
+          }
+        ).exec();
+      })
+      .value();
     res.send({});
   });
 
